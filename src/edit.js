6cd2d8f3d1356403bd64d7515b5656722cfef95d
Michal Staniecko <michal.staniecko@gmail.com>
2023-06-24 17:40:44 +0200

    configure stylelint

--- src/edit.js
+++ src/edit.js
@@ -30,10 +30,9 @@ import './editor.scss';
  * @return {WPElement} Element to render.
  */
 export default function Edit() {
-	const x = 0;
 	return (
-		<p {...useBlockProps()}>
-			{__('Boilerplate – hello from the editor!', 'boilerplate')}
+		<p { ...useBlockProps() }>
+			{ __( 'Boilerplate – hello from the editor!', 'boilerplate' ) }
 		</p>
 	);
 }
